e2320e8fbf80b7a2d53a416d3bc4ef8e84884dd5
Esthal <vova28201@mail.ru>
2023-09-26 20:12:26 +0700

    hashmap

--- backend/src/main/java/ru/anontmization/dataanonymizationtool/dto/mask/GeneralizationStringDto.java
+++ backend/src/main/java/ru/anontmization/dataanonymizationtool/dto/mask/GeneralizationStringDto.java
@@ -3,12 +3,12 @@ package ru.anontmization.dataanonymizationtool.dto.mask;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Data
 public class GeneralizationStringDto {
     private String nameTable;
     private String nameColumn;
     private String generalizationTable;
-    private ArrayList<String> valueFrom;
-    private ArrayList<String> valueTo;
+    private HashMap<String, String> value;
 }
